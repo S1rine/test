2cfba188b588d66e7fff0c5eeb99883237e3b340
Severus <1045725508@qq.com>
2019-10-18 13:39:37 +0800

    test

---
+++ ife5-6/common.js
@@ -0,0 +1,35 @@
+function checkvalue(x) {
+    if (x !== '') {
+        document.getElementsByTagName('button')[0].removeAttribute("disabled");
+    } else {
+        document.getElementsByTagName("button")[0].setAttribute("disabled", 'true');
+    }
+}
+var majors = [
+    ["major1", "major2", "major3", "major4"],
+    ["major5", "major6", "major7", "major8", "major9"],
+    ["major10", "major11"],
+    ["major12", "major13", "major14"],
+    ["major15", "major16", "major17", "major18"]
+];
+function checkschool(x) {
+    var selects = document.getElementById("student-major");
+    if (selects.childElementCount != 0) {
+        // for(var i=0;i<selects.childElementCount;i++){
+        //     selects.childNodes
+        // }
+        selects.options.length = 0;
+    }
+    var length = 0;
+    for (var i = 1; i < x; i++) {
+        length += majors[i - 1].length;
+        // console.log(length);
+    }
+    // console.log(length);
+    for (var i = 0; i < majors[x - 1].length; i++) {
+        var item = document.createElement("option");
+        item.value = length + i + 1;
+        item.innerHTML = majors[x - 1][i];
+        selects.appendChild(item);
+    }
+}
